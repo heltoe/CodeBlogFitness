5d8e7766154c3beedf4a63b0ca0e25fb623e1e37
Vlad Zhulinskiy <x-outhink@mail.ru>
2019-10-31 14:18:33 +0300

    Добавьте файлы проекта.

---
+++ CodeBlogFitness.BL/Model/Gender.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CodeBlogFitness.BL.Model
+{
+    /// <summary>
+    /// Gender.
+    /// </summary>
+    public class Gender
+    {
+        /// <summary>
+        /// Name.
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        ///  Create new gender
+        /// </summary>
+        /// <param name="name">Name gender.</param>
+        public Gender(string name)
+        {
+            if(string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Имя пола не может быть пустым или null", nameof(name));
+            }
+
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
